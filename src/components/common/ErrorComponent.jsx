92a3637f5f96de02a1966aff0c48e584a5c0e369
l0u0h0 <l0u0h0@gmail.com>
2024-03-04 22:12:20 +0900

    :Feature: Errorboundary 추가

---
+++ src/components/common/ErrorComponent.jsx
@@ -0,0 +1,14 @@
+const ErrorComponent = ({ error, resetErrorBoundary }) => {
+  if (error === undefined) {
+    return <div>알 수 없는 오류 발생</div>;
+  }
+  return (
+    <div>
+      {error.message}
+      <img src={`https://http.cat/${error.status}`} alt="Miyaong" />
+      <button onClick={resetErrorBoundary}>{error.message} 발생</button>
+    </div>
+  );
+};
+
+export default ErrorComponent;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 // Styled
 import GlobalStyles from "./styles/global.style";
 // WebVitals
 import reportWebVitals from "./reportWebVitals";
 // Component
 import CoursePage from "./pages/CoursePage";
+import ErrorComponent from "./components/common/ErrorComponent";
 // Jotai
 import { Provider } from "jotai";
 
@@ -19,12 +21,14 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <Provider>
-    <React.StrictMode>
-      <GlobalStyles />
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  </Provider>
+  <ErrorBoundary FallbackComponent={ErrorComponent}>
+    <Provider>
+      <React.StrictMode>
+        <GlobalStyles />
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    </Provider>
+  </ErrorBoundary>
 );
 
 reportWebVitals();
